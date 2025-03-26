bdf21cd67481842cee2cb26e7e1a983cf6479c10
Irhamdi Fahdiyan <irhamdy.fahdyan@gmail.com>
2025-03-26 10:35:14 +0700

    First Init

---
+++ breezepedia/breezepediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  breezepediaApp.swift
+//  breezepedia
+//
+//  Created by Muhammad Irhamdi Fahdiyan Noor on 26/03/25.
+//
+
+import SwiftUI
+
+@main
+struct breezepediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
