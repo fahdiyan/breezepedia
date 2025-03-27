49120d15f7b65f940e7ee0b038bd28dc6da5d5c3
Sabri Ramadhani <sabriramadhani886@gmail.com>
2025-03-27 08:45:28 +0700

    Added MenuCard list in TenantDetailView

--- breezepedia/TenantDetailView.swift
+++ breezepedia/TenantDetailView.swift
@@ -8,21 +8,81 @@
 import SwiftUI
 
 struct TenantDetailView: View {
+    private var menus = MenuModel.generateMenuModel()
+    
+    @State private var offset: CGFloat = 240
+
     var body: some View {
-        ZStack {
-            Color(.orange) // Change with Tenant Image here
-                .ignoresSafeArea()
-            GeometryReader { geometry in
-                VStack {
-                    Spacer() // Pushes the second layer down by 30%
-                    RoundedRectangle(cornerRadius: 30)
-                        .fill(Color.white)
-                        .frame(height: geometry.size.height * 0.7) // Covers 70%
-                        .shadow(radius: 5)
+        GeometryReader { geometry in
+            ZStack(alignment: .top) {
+                Image("tenant-1")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: geometry.size.width, height: 360)
+                
+                ScrollView {
+                    VStack(spacing: 0) {
+                        Color.clear
+                            .frame(height: max(offset, 0))
+                        
+                        ZStack {
+                            Color.white
+                                .frame(width: geometry.size.width)
+                                .clipShape(
+                                    RoundedRectangle(cornerRadius: 24)
+                                        .offset(x: 0, y: 0)
+                                )
+                                .shadow(radius: 4)
+                            
+                            VStack(alignment: .leading) {
+                                // Logo & Name
+                                
+                                // General Info
+                                Divider()
+                                    .frame(height: 1)
+                                    .overlay(.gray.opacity(0.3))
+                                    .padding(.vertical, 16)
+                                
+                                Text("Tenant Detail")
+                                    .frame(width: 132, height: 28)
+                                    .font(.system(size: 16, weight: .medium))
+                                    .foregroundColor(.white)
+                                    .background(.purple)
+                                    .clipShape(RoundedRectangle(cornerRadius: 5))
+                                    .padding(.bottom, 16)
+                                // ...
+                                
+                                
+                                // Menu List
+                                Divider()
+                                    .frame(height: 1)
+                                    .overlay(.gray.opacity(0.3))
+                                    .padding(.vertical, 16)
+                                
+                                Text("Menu List")
+                                    .frame(width: 132, height: 28)
+                                    .font(.system(size: 16, weight: .medium))
+                                    .foregroundColor(.white)
+                                    .background(.purple)
+                                    .clipShape(RoundedRectangle(cornerRadius: 5))
+                                    .padding(.bottom, 16)
+                                
+                                VStack(spacing: 12) {
+                                    ForEach(menus) { menu in
+                                        MenuCard(menu: menu)
+                                    }
+                                }
+                            }
+                            .padding(.vertical, 36)
+                            .padding(.horizontal, 32)
+                        }
+                    }
                 }
+                .scrollBounceBehavior(.basedOnSize)
             }
-            .ignoresSafeArea(edges: .bottom) // Ensure it extends to the bottom
         }
+        .ignoresSafeArea(.all, edges: .top)
+        .edgesIgnoringSafeArea(.bottom)
     }
 }
 
