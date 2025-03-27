2b31757551214156c48deee36bb90f9aeaca1cc5
Sabri Ramadhani <sabriramadhani886@gmail.com>
2025-03-27 11:53:05 +0700

    Add SearchField UI in Main Page

--- breezepedia/ContentView.swift
+++ breezepedia/ContentView.swift
@@ -9,13 +9,70 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            // MapKit
+            
+            VStack (alignment: .center) {
+                VStack {
+                    Spacer()
+                    HStack {
+                        Image("logo_breezepedia_white")
+                            .resizable()
+                            .frame(width: 80, height: 60)
+                        Spacer()
+                        SearchField()
+                    }
+                    .padding(.horizontal, 24)
+                    
+                    HStack {
+                        Text("Let’s explore ")
+                            .font(.system(size: 16, weight: .regular)) +
+                        Text("52 Tenants")
+                            .font(.system(size: 16, weight: .bold)) +
+                        Text(" at the Breeze!")
+                            .font(.system(size: 16, weight: .regular))
+                    }
+                    .foregroundColor(.white)
+                    .padding(.bottom, 14)
+                }
+                .frame(width: .infinity, height: 128)
+                .background(Color(UIColor(
+                    red: 0x70 / 255,
+                    green: 0x42 / 255,
+                    blue: 0x9A / 255,
+                    alpha: 1
+                )))
+                .padding(.top, 32)
+                .shadow(radius: 6, y: 3)
+                
+                Spacer()
+                
+                // Button Preferensi
+            }
+            .ignoresSafeArea(.all)
         }
-        .padding()
+    }
+}
+
+struct SearchField: View {
+    @State private var searchText: String = ""
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "magnifyingglass")
+                .foregroundColor(Color(UIColor(
+                    red: 0x70 / 255,
+                    green: 0x42 / 255,
+                    blue: 0x9A / 255,
+                    alpha: 1
+                )))
+                .padding(.leading, 10)
+
+            TextField("Search...", text: $searchText)
+        }
+        .frame(width: 248, height: 40)
+        .background(Color.white)
+        .cornerRadius(10)
     }
 }
 
