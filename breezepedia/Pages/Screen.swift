bbb2fbfb94d3bc54a211f5b3c6430022aab4d421
Rairavel <ravelinarais@gmail.com>
2025-04-03 05:36:19 -0700

    Create splash screen

---
+++ breezepedia/Pages/Screen.swift
@@ -0,0 +1,37 @@
+//
+//  Screen.swift
+//  breezepedia
+//
+//  Created by Raissa  on 03/04/25.
+//
+
+import SwiftUI
+
+struct ScreenView: View {
+    var body: some View {
+        ZStack {
+            Color(UIColor(
+                red: 0x70 / 255,
+                green: 0x42 / 255,
+                blue: 0x9A / 255,
+                alpha: 1
+            )).ignoresSafeArea()
+            
+            VStack {
+                Image("logo_breezepedia_white") 
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 300, height: 300)
+            }
+            Text("")
+                .foregroundColor(.white)
+                .font(.title)
+                .bold()
+        }
+    }
+}
+
+#Preview {
+    ScreenView()
+}
+
