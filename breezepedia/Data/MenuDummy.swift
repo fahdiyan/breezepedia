f2c62d0b911247dafa5aeefdd0b8d564a4cd68cb
Sabri Ramadhani <sabriramadhani886@gmail.com>
2025-04-08 08:08:38 +0700

    Add Tenant and Menu Dummy, again

---
+++ breezepedia/Data/MenuDummy.swift
@@ -0,0 +1,209 @@
+//
+//  MenuDummy.swift
+//  breezepedia
+//
+//  Created by Sabri Ramadhani on 07/04/25.
+//
+
+let dummyMenus: [String: [MenuModel]] = [
+    "J.Co": [
+        MenuModel(
+            name: "Jcochino",
+            image: "jcochino",
+            description: "Double shot espresso + milk",
+            price: 27000
+        ),
+        MenuModel(
+            name: "Matcha Frappe",
+            image: "greenteafrappe",
+            description: "Premium matcha powder + milk, garnished with whipped cream",
+            price: 35000
+        ),
+        MenuModel(
+            name: "Strawberry Yogurt Frappe",
+            image: "strawberryfrappe",
+            description: "Lorem ipsum dolor sit amet",
+            price: 35000
+        ),
+        MenuModel(
+            name: "Jcoccino Frappe",
+            image: "jcoccinofrappe",
+            description: "Lorem ipsum dolor sit amet",
+            price: 35000
+        ),
+        MenuModel(
+            name: "Iced Lemon Tea",
+            image: "lemontea",
+            description: "Lorem ipsum dolor sit amet",
+            price: 21000
+        ),
+        MenuModel(
+            name: "Alcapoon",
+            image: "alcapoon",
+            description: "Soft donut dough with slices of almond",
+            price: 10000
+        ),
+        MenuModel(
+            name: "Rainbow Choco",
+            image: "rainbowchoco",
+            description: "Soft donut dough with rainbow sprinkles on top",
+            price: 10000
+        ),
+        MenuModel(
+            name: "Choco Caviar",
+            image: "caviarchoco",
+            description: "Soft donut dough with small crunchy choco balls",
+            price: 10000
+        )
+    ],
+    "bebek bengil": [
+        MenuModel(
+            name: "Menu",
+            image: "strawberryfrappe",
+            description: "Lorem ipsum dolor sit amet",
+            price: 25000
+        ),
+        MenuModel(
+            name: "Menu",
+            image: "strawberryfrappe",
+            description: "Lorem ipsum dolor sit amet",
+            price: 25000
+        ),
+        MenuModel(
+            name: "Menu",
+            image: "strawberryfrappe",
+            description: "Lorem ipsum dolor sit amet",
+            price: 25000
+        ),
+        MenuModel(
+            name: "Menu",
+            image: "strawberryfrappe",
+            description: "Lorem ipsum dolor sit amet",
+            price: 25000
+        ),
+        MenuModel(
+            name: "Menu",
+            image: "strawberryfrappe",
+            description: "Lorem ipsum dolor sit amet",
+            price: 25000
+        ),
+        MenuModel(
+            name: "Menu",
+            image: "strawberryfrappe",
+            description: "Lorem ipsum dolor sit amet",
+            price: 25000
+        ),
+        MenuModel(
+            name: "Menu",
+            image: "strawberryfrappe",
+            description: "Lorem ipsum dolor sit amet",
+            price: 25000
+        ),
+        MenuModel(
+            name: "Menu",
+            image: "strawberryfrappe",
+            description: "Lorem ipsum dolor sit amet",
+            price: 25000
+        )
+    ],
+    "Burgreens": [
+        MenuModel(
+            name: "Menu",
+            image: "strawberryfrappe",
+            description: "Lorem ipsum dolor sit amet",
+            price: 25000
+        ),
+        MenuModel(
+            name: "Menu",
+            image: "strawberryfrappe",
+            description: "Lorem ipsum dolor sit amet",
+            price: 25000
+        ),
+        MenuModel(
+            name: "Menu",
+            image: "strawberryfrappe",
+            description: "Lorem ipsum dolor sit amet",
+            price: 25000
+        ),
+        MenuModel(
+            name: "Menu",
+            image: "strawberryfrappe",
+            description: "Lorem ipsum dolor sit amet",
+            price: 25000
+        ),
+        MenuModel(
+            name: "Menu",
+            image: "strawberryfrappe",
+            description: "Lorem ipsum dolor sit amet",
+            price: 25000
+        ),
+        MenuModel(
+            name: "Menu",
+            image: "strawberryfrappe",
+            description: "Lorem ipsum dolor sit amet",
+            price: 25000
+        ),
+        MenuModel(
+            name: "Menu",
+            image: "strawberryfrappe",
+            description: "Lorem ipsum dolor sit amet",
+            price: 25000
+        ),
+        MenuModel(
+            name: "Menu",
+            image: "strawberryfrappe",
+            description: "Lorem ipsum dolor sit amet",
+            price: 25000
+        )
+    ],
+    "Boost": [
+        MenuModel(
+            name: "Menu",
+            image: "strawberryfrappe",
+            description: "Lorem ipsum dolor sit amet",
+            price: 25000
+        ),
+        MenuModel(
+            name: "Menu",
+            image: "strawberryfrappe",
+            description: "Lorem ipsum dolor sit amet",
+            price: 25000
+        ),
+        MenuModel(
+            name: "Menu",
+            image: "strawberryfrappe",
+            description: "Lorem ipsum dolor sit amet",
+            price: 25000
+        ),
+        MenuModel(
+            name: "Menu",
+            image: "strawberryfrappe",
+            description: "Lorem ipsum dolor sit amet",
+            price: 25000
+        ),
+        MenuModel(
+            name: "Menu",
+            image: "strawberryfrappe",
+            description: "Lorem ipsum dolor sit amet",
+            price: 25000
+        ),
+        MenuModel(
+            name: "Menu",
+            image: "strawberryfrappe",
+            description: "Lorem ipsum dolor sit amet",
+            price: 25000
+        ),
+        MenuModel(
+            name: "Menu",
+            image: "strawberryfrappe",
+            description: "Lorem ipsum dolor sit amet",
+            price: 25000
+        ),
+        MenuModel(
+            name: "Menu",
+            image: "strawberryfrappe",
+            description: "Lorem ipsum dolor sit amet",
+            price: 25000
+        )
+    ]
+]
